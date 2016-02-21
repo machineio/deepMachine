7eaf82933c344caa27099d236d59944052bed23c
ulillo <riveraulises1@gmail.com>
2016-02-20 18:30:09 -0600

    test

--- static/js/seed/fun.strings.js
+++ static/js/seed/fun.strings.js
@@ -912,11 +912,15 @@ fun.strings = {
     systemMonitor: "System Monitor",
     goodBye: "Good bye",
     asterisk: "*",
+
     speakingWithOur: "SPEAK WITH OUR MARKET RISK ANALYSTS",
     trainingCoursesToMaximize: "1 on 1 Training courses to maximize your returns. And insure a realistic learning curve.",
+
     radeWithMarketProfessionals: "RADE WITH MARKET PROFESSIONALS",
-    safeAndSecureTrading: "100% safe and secure trading courses. Top quality material To help you become a professional Trader",
+    safeAndSecureTrading: "100% safe and secure trading courses. Top quality material. To help you become a professional Trader",
     
+    radeWithMarketProfessionals: "RADE WITH MARKET PROFESSIONALS",
+    safeAndSecureTrading: "100% safe and secure trading courses. Top quality material. To help you become a professional Trader",
     
 
 
