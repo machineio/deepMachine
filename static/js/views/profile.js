8027c324ec536534233535dcf7dcb5d3cb11c9e2
Jean Chassoul <chassoul@gmail.com>
2016-02-23 17:38:36 -0800

    no

--- static/js/views/profile.js
+++ static/js/views/profile.js
@@ -331,6 +331,7 @@ fun.views.profile = Backbone.View.extend({
             callbacks,
             validForm;
         event.preventDefault();
+        
         signupError = this.signupError;
         asset = this.asset.val();
         expiry = this.expiry.val();
@@ -339,26 +340,7 @@ fun.views.profile = Backbone.View.extend({
         console.log(asset, expiry, amount);
         // check if this view stuff is really needed
         view = this;
-        // form validation rules
-        rules = {
-            rules: {
-                profile_first_asset: {
-                    required: true
-                },
-                profile_first_amount: {
-                    required: true,
-                    minlength: 3,
-                    email: true
-                },
-                profile_first_expiry: {
-                    //minlength: 8,
-                    required: true
-                }
-            }
-        }
-        validationRules = $.extend(rules, fun.utils.validationRules);
-
-        $('#profile-first-trade-form').validate(validationRules);
+        
 
 
         $("#signupForm").validate({
@@ -418,83 +400,6 @@ fun.views.profile = Backbone.View.extend({
             },
             unhighlight: function (element, errorClass, validClass) {
                 $( element ).parents( ".col-sm-5" ).addClass( "has-success" ).removeClass( "has-error" );
-            }
-        });
-
-
-        
-        $( "#signupForm1" ).validate( {
-            rules: {
-                firstname1: "required",
-                lastname1: "required",
-                username1: {
-                    required: true,
-                    minlength: 2
-                },
-                password1: {
-                    required: true,
-                    minlength: 5
-                },
-                confirm_password1: {
-                    required: true,
-                    minlength: 5,
-                    equalTo: "#password1"
-                },
-                email1: {
-                    required: true,
-                    email: true
-                },
-                agree1: "required"
-            },
-            messages: {
-                firstname1: "Please enter your firstname",
-                lastname1: "Please enter your lastname",
-                username1: {
-                    required: "Please enter a username",
-                    minlength: "Your username must consist of at least 2 characters"
-                },
-                password1: {
-                    required: "Please provide a password",
-                    minlength: "Your password must be at least 5 characters long"
-                },
-                confirm_password1: {
-                    required: "Please provide a password",
-                    minlength: "Your password must be at least 5 characters long",
-                    equalTo: "Please enter the same password as above"
-                },
-                email1: "Please enter a valid email address",
-                agree1: "Please accept our policy"
-            },
-            errorElement: "em",
-            errorPlacement: function ( error, element ) {
-                // Add the `help-block` class to the error element
-                error.addClass( "help-block" );
-                // Add `has-feedback` class to the parent div.form-group
-                // in order to add icons to inputs
-                element.parents( ".col-sm-5" ).addClass( "has-feedback" );
-                if ( element.prop( "type" ) === "checkbox" ) {
-                    error.insertAfter( element.parent( "label" ) );
-                } else {
-                    error.insertAfter( element );
-                }
-                // Add the span element, if doesn't exists, and apply the icon classes to it.
-                if ( !element.next( "span" )[ 0 ] ) {
-                    $( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
-                }
-            },
-            success: function ( label, element ) {
-                // Add the span element, if doesn't exists, and apply the icon classes to it.
-                if ( !$( element ).next( "span" )[ 0 ] ) {
-                    $( "<span class='glyphicon glyphicon-ok form-control-feedback'></span>" ).insertAfter( $( element ) );
-                }
-            },
-            highlight: function ( element, errorClass, validClass ) {
-                $( element ).parents( ".col-sm-5" ).addClass( "has-error" ).removeClass( "has-success" );
-                $( element ).next( "span" ).addClass( "glyphicon-remove" ).removeClass( "glyphicon-ok" );
-            },
-            unhighlight: function ( element, errorClass, validClass ) {
-                $( element ).parents( ".col-sm-5" ).addClass( "has-success" ).removeClass( "has-error" );
-                $( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
             }
         });
 
