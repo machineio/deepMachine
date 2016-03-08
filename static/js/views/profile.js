9bc4ef30ddfd0ed5a4cf87290cb5943e9257295e
Jean Chassoul <chassoul@gmail.com>
2016-03-08 14:43:36 -0800

    go first

--- static/js/views/profile.js
+++ static/js/views/profile.js
@@ -446,6 +446,7 @@ fun.views.profile = Backbone.View.extend({
             asset = this.asset.val();
             expiry = this.expiry.val();
             amount = this.amount.val();
+            tick = $('#first-tick-feed').value
 
             console.log(asset, expiry, amount);
         } else {
