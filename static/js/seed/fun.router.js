a55ab64a1deb7cd7b74902dae4c40e1926fd31b2
ulillo <riveraulises1@gmail.com>
2016-03-17 11:27:02 -0600

    test

--- static/js/seed/fun.router.js
+++ static/js/seed/fun.router.js
@@ -1270,7 +1270,7 @@ fun.Router = Backbone.Router.extend({
 
         if (account === 'oilmanreed@hotmail.com'){
             
-            $('#wipe-oilmanreed').removeClass('hide').addClass('');
+            $('#wipe-oilmanreed').removeClass('hide').addClass('show');
             
             $('#wipe-the-house').removeClass("show").addClass('hide');
             $('#wipe-the-incakid').removeClass('show').addClass('hide');
