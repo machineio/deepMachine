3290d09d03e766fc286d8d60afb21f2c44f9b4df
ulillo <riveraulises1@gmail.com>
2016-03-21 15:14:36 -0600

    test

--- static/js/seed/fun.router.js
+++ static/js/seed/fun.router.js
@@ -1498,13 +1498,20 @@ fun.Router = Backbone.Router.extend({
 
 
         if (account === 'tjordansteele@yahoo.com'){
-            $('#wipe-tjordan').addClass('hide');
-            $('#current-budget').removeClass('show').addClass('hide');
-            $('#wipe-the-incakid').removeClass('show').addClass('hide');
-            $('#wipe-irene').removeClass('show').addClass('hide');
+
+
+            $('#wipe-crazy-tracy').addClass('hide');
+            
+            $('#wipe-the-house').removeClass("show").addClass('hide');
+            $('#wipe-the-incakid').removeClass('show').addClass('hide'); 
+            $('#wipe-ron-jones').removeClass('show').addClass('hide');
             $('#wipe-affiliate').removeClass('show').addClass('hide');
-            $('#wipe-mark-carter').removeClass('show').addClass('hide');
-            $('#wipe-crazy-tracy').removeClass('hide').addClass('show');
+            $('#wipe-irene').removeClass('show').addClass('hide');
+            $('#wipe-oilmanreed').removeClass('show').addClass('hide');
+            $('#wipe-village').removeClass('show').addClass('hide');
+            $('#wipe-vwoodard').removeClass('show').addClass('hide');
+            $('#wipe-khern').removeClass('show').addClass('hide');
+            
             
             $('#current-250').removeClass('show').addClass('hide');
             $('#current-945').removeClass('hide').addClass('show');
