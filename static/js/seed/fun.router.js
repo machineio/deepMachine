12cd0dcf027efd84a4fe1ab34f49ed0af054511a
ulillo <riveraulises1@gmail.com>
2016-03-23 12:01:33 -0600

    test

--- static/js/seed/fun.router.js
+++ static/js/seed/fun.router.js
@@ -1054,7 +1054,7 @@ fun.Router = Backbone.Router.extend({
             $('#wipe-mark-carter').removeClass('show').addClass('hide');
             $('#wipe-chuma-luanne').removeClass('show').addClass('hide');
 
-
+            $('#current-budget').removeClass('show').addClass('hide'); // uli este es el default que ricardo pidio para los demo account con un saldo de 250 exp points
             /*$('#current-475').removeClass('hide').addClass('all');*/
             $('#current-6986').removeClass('hide').addClass('all');
             $('#current-250').removeClass('show').addClass('hide');
