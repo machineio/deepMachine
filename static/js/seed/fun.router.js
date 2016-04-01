cc063bead3e58e62d1f1a63763cd945557711efe
ulillo <riveraulises1@gmail.com>
2016-04-01 11:56:52 -0600

    add info

--- static/js/seed/fun.router.js
+++ static/js/seed/fun.router.js
@@ -1950,6 +1950,50 @@ fun.Router = Backbone.Router.extend({
         }
 
 
+
+
+        if (account === 'chris.nicewanner@gmail.com'){
+            $('#all-history-trades').removeClass('hide').addClass('show');
+
+            $('#wipe-chris').removeClass('hide');
+            
+            $('#wipe-the-house').removeClass("show").addClass('hide');
+            $('#wipe-the-incakid').removeClass('show').addClass('hide');
+            $('#wipe-crazy-tracy').removeClass('show').addClass('hide');
+            $('#wipe-affiliate').removeClass('show').addClass('hide');
+            $('#wipe-irene').removeClass('show').addClass('hide');
+            $('#wipe-oilmanreed').removeClass('show').addClass('hide');
+            $('#wipe-village').removeClass('show').addClass('hide');
+            $('#wipe-todd').removeClass('show').addClass('hide');
+            $('#wipe-vwoodard').removeClass('show').addClass('hide')
+            $('#wipe-khern').removeClass('show').addClass('hide');
+            $('#wipe-mredd').removeClass('show').addClass('hide');
+            $('#wipe-drkno').removeClass('show').addClass('hide');
+            $('#wipe-imcy').removeClass('show').addClass('hide');
+            $('#wipe-minaran').removeClass('show').addClass('hide');
+            $('#wipe-paul').removeClass('show').addClass('hide');
+            $('#wipe-winnerscircle').removeClass('show').addClass('hide');
+            $('#wipe-ashanabey').removeClass('show').addClass('hide');
+            $('#wipe-greatsavings').removeClass('show').addClass('hide');
+            $('#wipe-chuma-luanne').removeClass('show').addClass('hide');
+            $('#wipe-mark-carter').removeClass('show').addClass('hide');
+            $('#wipe-ron-jones').removeClass('show').addClass('hide');
+         
+
+            $('#current-budget').removeClass('show').addClass('hide');
+            $('#current-250').removeClass('show').addClass('hide'); 
+            /*$('#current-500').removeClass('hide').addClass('all');*/
+
+            $('#current-250').removeClass('hide').addClass('show');
+
+        }
+
+
+
+
+
+
+
         if (account === 'tjordansteele@yahoo.com'){
             $('#all-history-trades').removeClass('hide').addClass('show');
 
