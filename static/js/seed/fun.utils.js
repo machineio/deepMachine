7a8dd2b4e273229f1df53290c5404d391409bdc8
Natalia Aguirre <natyaguirro@gmail.com>
2016-04-13 14:35:48 -0600

    fixrouter

--- static/js/seed/fun.utils.js
+++ static/js/seed/fun.utils.js
@@ -80,10 +80,6 @@ fun.utils.hideAmounts = function(){
     $('#current-666').removeClass('show').addClass('hide');
     $('#current-1126').removeClass('show').addClass('hide');
     $('#current-4257').removeClass('show').addClass('hide');
-    $('#current-250').removeClass('show').addClass('hide');
-    $('#current-602').removeClass('show').addClass('hide');
-    $('#current-652').removeClass('show').addClass('hide');
-    $('#current-250').removeClass('show').addClass('hide');
     $('#current-602').removeClass('show').addClass('hide');
     $('#current-652').removeClass('show').addClass('hide');
     $('#current-879').removeClass('show').addClass('hide');
