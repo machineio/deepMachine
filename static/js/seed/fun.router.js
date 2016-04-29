3badc24c1d7c6a5b74e1a9ffa64ddc914f39314c
ulillo <riveraulises1@gmail.com>
2016-04-29 12:35:44 -0600

    add info

--- static/js/seed/fun.router.js
+++ static/js/seed/fun.router.js
@@ -1188,7 +1188,7 @@ fun.Router = Backbone.Router.extend({
             fun.utils.hideAmounts();
             fun.utils.hideAccountsHistory();
             $('#wipe-oilmanreed').removeClass('hide');
-            $('#current-2233').removeClass('hide').addClass('show');
+            $('#current-2284').removeClass('hide').addClass('show');
 
 
         }
